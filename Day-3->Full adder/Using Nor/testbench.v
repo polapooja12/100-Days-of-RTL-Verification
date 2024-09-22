b8785a708e02b92a2aec7b85dfab6bfabaf90ef5
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 20:06:31 +0530

    Update testbench.v

--- Day-3->Full adder/Using Nor/testbench.v
+++ Day-3->Full adder/Using Nor/testbench.v
@@ -1,1 +1,20 @@
-
+`include "design.v"
+module fa_nor_tb(a,b,cin,sum,cout);
+  input wire sum,cout;
+  output reg a,b,cin;
+  fa_nor dut(a,b,cin,sum,cout);
+  initial begin
+    $monitor("Time=%0t a=%0b b=%0b cin=%0b sum=%0b cout=%0b",$time,a,b,cin,sum,cout);
+    #5 a=0;b=0;cin=0;
+    #5 a=0;b=0;cin=1;
+    #5 a=0;b=1;cin=0;
+    #5 a=0;b=1;cin=1;
+    #5 a=1;b=0;cin=0;
+    #5 a=1;b=0;cin=1;
+    #5 a=1;b=1;cin=0;
+    #5 a=1;b=1;cin=1;
+    #5
+    $finish;
+  end
+endmodule
+    
