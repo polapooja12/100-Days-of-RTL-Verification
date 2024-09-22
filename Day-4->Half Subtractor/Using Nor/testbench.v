24b362b0f2b4ba7fb495b5690c5145e0ae6b5c51
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 20:22:32 +0530

    Update testbench.v

--- Day-4->Half Subtractor/Using Nor/testbench.v
+++ Day-4->Half Subtractor/Using Nor/testbench.v
@@ -1,1 +1,15 @@
-
+`include "half_nor.v"
+module half_nor_test(differ,borrow,a,b);
+output reg a,b;
+input wire differ,borrow;
+  half_nor inst(.differ(differ), borrow(borrow), .a(a), .b(b));
+  initial begin
+    $monitor("Time=%0t => a=%b b=%b difference=%b borrow=%b",$time,a,b,differ,borrow);
+      #1 a=0;b=0;
+      #1 a=0;b=1;
+      #1 a=1;b=0;
+      #1 a=1;b=1;
+      #1
+      $finish;
+    end
+endmodule
