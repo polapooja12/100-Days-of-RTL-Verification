ce7b2476e1e7b53a1108b98941e3fb1aa922eec9
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 19:51:18 +0530

    Update design.v

--- Day-2->Half adder/Using Behaviour/design.v
+++ Day-2->Half adder/Using Behaviour/design.v
@@ -1,1 +1,7 @@
-
+/* used xor gate for sum and and gate for carry by behaviour model*/
+module halfadder(input a,b,output reg sum,carry);
+  always @*begin
+     sum=a^b;
+     carry=a&b;
+  end
+endmodule
