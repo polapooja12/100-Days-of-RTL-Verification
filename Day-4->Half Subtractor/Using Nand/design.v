28eecb432dad377605958cc3bba57d171d38239b
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 20:21:08 +0530

    Update design.v

--- Day-4->Half Subtractor/Using Nand/design.v
+++ Day-4->Half Subtractor/Using Nand/design.v
@@ -1,1 +1,8 @@
-
+module half_nand(input wire a,b,output reg differ,borrow);
+  wire w1,w2,w3;
+  assign w1=~(a&b);
+  assign w2=~(a&w1);
+  assign w3=~(b&w1);
+  assign differ=~(w1&w2);
+  assign borrow=~(w3&w3);
+endmodule
