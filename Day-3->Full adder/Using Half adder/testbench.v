067576c4d26141389f5e16995c02919e233c40e3
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 20:05:01 +0530

    Update testbench.v

--- Day-3->Full adder/Using Half adder/testbench.v
+++ Day-3->Full adder/Using Half adder/testbench.v
@@ -1,1 +1,19 @@
-
+`include "design.v"
+module tb_top;
+reg a,b,c_in;
+wire s_out, c_out;
+full_adder inst_a(a, b, c_in, s_out, c_out);
+initial begin
+    $monitor("Time=%0t a=%b b=%b c_in=%b s_out=%b c_out=%b", $time, a, b, c_in, s_out, c_out);
+    #5 a=0;b=0;cin=0;
+    #5 a=0;b=0;cin=1;
+    #5 a=0;b=1;cin=0;
+    #5 a=0;b=1;cin=1;
+    #5 a=1;b=0;cin=0;
+    #5 a=1;b=0;cin=1;
+    #5 a=1;b=1;cin=0;
+    #5 a=1;b=1;cin=1;
+    #5
+    $finish;
+  end
+endmodule
