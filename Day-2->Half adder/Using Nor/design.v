beea24e6f2536817ef5df81126c5d71f0a77903e
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 19:53:17 +0530

    Update design.v

--- Day-2->Half adder/Using Nor/design.v
+++ Day-2->Half adder/Using Nor/design.v
@@ -1,1 +1,12 @@
+/*using nor gates when one input is high the output will be zero*/
+
+module ha_nor(input a,b,output reg carry);
+  wire w1,w2,w3;
+  assign w1=~(a|a);
+  assign w2=~(b|b);
+  assign  w3=~(a|b);
+  assign carry=~(w1|w2);
+  assign summ=~(carry|w3);
+endmodule
+
 
