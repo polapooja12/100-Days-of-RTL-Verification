41ea687102c4f04df4f9b88f188374d2743f2da8
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 19:52:07 +0530

    Update design.v

--- Day-2->Half adder/Using NAND/design.v
+++ Day-2->Half adder/Using NAND/design.v
@@ -1,1 +1,11 @@
+/*using nand gates complement of and gate*/
+
+module ha_nand(input a,b,output reg carry);
+  wire w1,w2,w3;
+  assign w1=~(a&b);
+  assign w2=~(w1&a);
+  assign  w3=~(w1&b);
+  assign carry=~(w1&w1);
+  assign summ=~(w2&w3);
+endmodule
 
