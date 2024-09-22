431b8bc4271b05c07c7c2677e2ffa7a2527a4ff3
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-09-22 20:22:10 +0530

    Update design.v

--- Day-4->Half Subtractor/Using Nor/design.v
+++ Day-4->Half Subtractor/Using Nor/design.v
@@ -1,1 +1,8 @@
-
+module half_nor(input wire a,b,output reg differ,borrow);
+  wire w1,w2,w3,w4;
+  assign w1=~(a|b);
+  assign borrow=~(a|w1);
+  assign w2=~(b|w1);
+  assign w3=~(borrow|w2);
+  assign differ=~(w3|w3);
+endmodule
