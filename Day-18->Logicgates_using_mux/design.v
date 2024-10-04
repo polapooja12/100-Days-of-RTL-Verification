0f8d8970e24e169abf7ef8eb4a8fa61d780c2fe3
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-04 12:43:11 +0530

    Update design.v

--- Day-18->Logicgates_using_mux/design.v
+++ Day-18->Logicgates_using_mux/design.v
@@ -1,1 +1,11 @@
-
+module logicgates_using_mux(input wire a,b, output reg y1,y2,y3,y4,y5,y6);
+  mux_2x1 and_gate(1'b0,b,a,yl);
+  mux_2x1 nand_gate(1'b1,~b,a,y2);
+  mux_2x1 or_gate(b,bl,a,y3);
+  mux_2x1 nor_gate(~b,1'b0,a,y4);
+  mux_2x1 xor_gate(b,~b,a,y5);
+  mux_2x1 xnor_gate(~b,b,a,уб);
+endmodule
+module mux_2x1(input a,b,sel, output y);
+  assign y=(a&~sel)|(b&sel);
+endmodule
