35a46ba669ce18822b574a723d9ed19eef552a59
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-09 21:54:06 +0530

    Update design.v

--- Day-23->JK-Flipflop/Using SRff/design.v
+++ Day-23->JK-Flipflop/Using SRff/design.v
@@ -1,14 +1,20 @@
-module dff(
-    input clk, reset, d,
-    output reg Q
-    );
+module srff(
+    input clk,reset,S,R, 
+    output reg Q);
     
     always@(posedge clk)
           begin
-            if({reset})
-            Q<= 1'b0;
-            else 
-            Q <= d;
-            end
+            if(reset)
+            Q <= 1'b0;
+            else
+                begin
+                case({S,R})
+                2'b00:Q<=Q;
+                2'b01:Q<=1'b0;
+                2'b10:Q<=1'b1;
+                default: Q<= 2'bxx;
+                endcase
+                end          
+         end
 
 endmodule
