ec85581d8f93aec9db41db245cba65c0f359cd73
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-09 21:49:23 +0530

    Update design.v

--- Day-23->JK-Flipflop/Using SRff/design.v
+++ Day-23->JK-Flipflop/Using SRff/design.v
@@ -1,1 +1,14 @@
+module dff(
+    input clk, reset, d,
+    output reg Q
+    );
+    
+    always@(posedge clk)
+          begin
+            if({reset})
+            Q<= 1'b0;
+            else 
+            Q <= d;
+            end
 
+endmodule
