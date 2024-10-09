8d398e2f5a76307b74e83ae5747c0c6f3635c2a6
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-09 21:50:37 +0530

    Update design.v

--- Day-23->JK-Flipflop/using Dff/design.v
+++ Day-23->JK-Flipflop/using Dff/design.v
@@ -1,1 +1,17 @@
-
+module tff(
+    input t,clk,reset,
+    output reg Q
+    );
+    always@(posedge clk)
+          begin
+            if(reset)
+               Q <= 1'b0;
+            else
+                begin
+                   if(t)
+                    Q<= ~Q;
+                   else
+                    Q<= Q;                     
+                end
+          end
+endmodule
