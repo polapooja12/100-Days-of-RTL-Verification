969c691939c8e045a6696bd9d39e0031710d5dae
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-10 16:59:48 +0530

    Update design.v

--- Day-24->D-flipflop/D using SR, JK, T FLIPFLOPS/design.v
+++ Day-24->D-flipflop/D using SR, JK, T FLIPFLOPS/design.v
@@ -1,1 +1,10 @@
-
+`include "sr_flipflop.v"
+`include "jk_flipflop.v"
+`include "t_flipflop.v"
+module dff_using_sr_jk_t(input clk, reset, d, output q_sr,q_jk,q_t );
+  wire w;
+  sr_flipflop sr(clk, reset, d, ~d, q_sr);
+  jk_flipflop jk(d, ~d, clk, reset, q_jk);
+  assign w= d^q_t;
+  t_flipflop t(w,clk,reset,q_t);  
+endmodule
