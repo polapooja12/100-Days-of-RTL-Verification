333132c1ac3f78fda1e8de8a1629cb080715988f
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-10 17:02:40 +0530

    Update design.v

--- Day-24->D-flipflop/Using JKff/design.v
+++ Day-24->D-flipflop/Using JKff/design.v
@@ -1,1 +1,19 @@
-
+module jk_flipflop(
+    input j,k,clk,reset,
+    output reg q
+    );
+    always@(posedge clk)
+          begin
+            if({reset})
+            q <= 1'b0;
+            else
+                begin
+                case({j,k})
+                2'b00:q<=q;
+                2'b01:q<=1'b0;
+                2'b10:q<=1'b1;
+                2'b11:q<=~q;
+                endcase
+                end          
+         end
+endmodule
