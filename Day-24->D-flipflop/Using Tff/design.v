1cbfc356b0b46dd92eecdb517a3b9f2eabb45059
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-10 17:01:19 +0530

    Update design.v

--- Day-24->D-flipflop/Using Tff/design.v
+++ Day-24->D-flipflop/Using Tff/design.v
@@ -1,1 +1,18 @@
 
+module t_flipflop(
+    input t,clk,reset,
+    output reg Q
+    );
+    always@(posedge clk)
+          begin
+            if(reset)
+               Q <= 1'b0;
+            else
+                begin
+                   if(t)
+                    Q<= ~Q;
+                   else
+                    Q<= Q;                     
+                end
+          end
+endmodule
