f4859063b9849e6b11bbf8f943d8cffa2a6dc1fd
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-12 12:14:57 +0530

    Update design.v

--- Day-25->T-flipflop/using dff/design.v
+++ Day-25->T-flipflop/using dff/design.v
@@ -1,28 +1,14 @@
-module testbench_tb;
-reg clk,rst,t;
-wire Q_sr, Q_jk, Q_d;
-  tffusingsr_jk_d dut(clk, rst, t, Q_sr, Q_jk, Q_d);
-  
-  initial 
-  begin 
-       clk=0;
-       t=0;
-       forever #4 clk=~clk;
-  end
+module dff(
+    input clk, reset, d,
+    output reg Q
+    );
     
-  initial 
-      begin
-          rst=1;
-          #10;
-          rst=0;
-          forever
-          begin   
-          #10 t = 1'b1;
-          #20  t = 1'b0; 
-          end
-      end
-    initial begin
-    $monitor("\t clock: %b  T: %b  Q_sr: %d  Q_jk: %d  Q_d: %b",clk,t,Q_sr, Q_jk, Q_d);
-    #100$finish;
-    end
+    always@(posedge clk)
+          begin
+            if({reset})
+            Q<= 1'b0;
+            else 
+            Q <= d;
+            end
+
 endmodule
