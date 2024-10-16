e1bb2ebd127e8495aba1653912d8e3f5f91c41fc
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-16 23:11:59 +0530

    Update testbench.v

--- Day-30->Synchronous Counter/testbench.v
+++ Day-30->Synchronous Counter/testbench.v
@@ -1,1 +1,12 @@
-
+`include "d_suync_counter.v"
+module d-sync_counter_test(input wire[3:0]q,qbar, output reg clk);
+  d_sync_counter inst(.clk(clk), .q(q), .qbar(qbar));
+  always #10 clk=~clk;
+  initial begin
+    $monitor("Time=%0t=>q=%d qbar=%d",$time,q,qbar);
+    clk=0;
+    #10;
+    #350;
+    $finish;
+  end
+endmodule
