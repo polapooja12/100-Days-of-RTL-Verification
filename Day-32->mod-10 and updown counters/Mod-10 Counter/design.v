1bc3d9ac185b865716357fc500309390cdd52dbb
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-18 23:01:25 +0530

    Update design.v

--- Day-32->mod-10 and updown counters/Mod-10 Counter/design.v
+++ Day-32->mod-10 and updown counters/Mod-10 Counter/design.v
@@ -1,1 +1,10 @@
-
+module mod10counter(clk,out);
+  input wire clk;
+  output reg [3:0] out=4'b000;
+  always @(posedge clk) begin
+    if(out==10)
+      out=0;
+    else
+      out=out+1;
+  end
+endmodule
