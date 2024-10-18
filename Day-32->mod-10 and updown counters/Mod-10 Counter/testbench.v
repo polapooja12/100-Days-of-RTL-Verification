cb2425a8d0fa8e0b673ce1d079db075b32714dc8
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-18 23:01:50 +0530

    Update testbench.v

--- Day-32->mod-10 and updown counters/Mod-10 Counter/testbench.v
+++ Day-32->mod-10 and updown counters/Mod-10 Counter/testbench.v
@@ -1,1 +1,13 @@
-
+`include "modncounter.v"
+module modncounter_test;
+  wire [3:0] out;
+  reg clk;
+  modncounter inst(clk,out);
+  always #20 clk=~clk;
+  initial begin
+    $monitor("Time=%t out=%d",$time,out);
+    clk=0;
+    #500;
+    $finish;
+  end
+endmodule
