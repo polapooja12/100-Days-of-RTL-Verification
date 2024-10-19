f747b370f0087827e44f34a491400c94b31fffe5
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-19 16:23:53 +0530

    Update testbench.v

--- Day-33->Johnson and Ring Counters/Johnson Counter/testbench.v
+++ Day-33->Johnson and Ring Counters/Johnson Counter/testbench.v
@@ -5,7 +5,7 @@ module johnson_counter_test(q,qbar,clk,reset);
   johnson_counter inst(q,qbar,clk,reset);
   always #10 clk=~clk;
   initial begin
-    $monitor("Time=%0t=>q=$04b",$time,q);
+    $monitor("Time=%0t=>q=%04b",$time,q);
     clk=0;
     #10 reset=1;
     #20 reset=0;
