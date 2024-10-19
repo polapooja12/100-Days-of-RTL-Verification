8e377f58169be4f945c2c0a84814d90862b14a91
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-19 16:22:13 +0530

    Update design.v

--- Day-33->Johnson and Ring Counters/Johnson Counter/design.v
+++ Day-33->Johnson and Ring Counters/Johnson Counter/design.v
@@ -15,7 +15,7 @@ endmodule
 module johnson_counter(q,qbar,clk,reset);
   input wire clk,reset;
   output reg [3:0] q,qbar;
-  dff d1(q[0].qbar[0],qbar[3],clk,reset);
+  dff d1(q[0],qbar[0],qbar[3],clk,reset);
   dff d2(q[1],qbar[1],q[0],clk,reset);
   dff d3(q[2],qbar[2],q[1],clk,reset);
   dff d4(q[3],qbar[3],q[2],clk,reset);
