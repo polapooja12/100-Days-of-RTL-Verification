a6db3dbff19041c9494e2d09d3a36b72b57b3281
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-21 15:47:16 +0530

    Update testbench.v

--- Day-34-Clock Phase and Clock Edge Detection/Clock Phase/testbench.v
+++ Day-34-Clock Phase and Clock Edge Detection/Clock Phase/testbench.v
@@ -1,1 +1,20 @@
-
+`include "clock_phase.v"
+module clock_phase_test;
+  reg clk;
+  reg rst;
+  wire clk_0;
+  wire clk_90;
+  wire clk_180;
+  wire clk_270;
+  clock_phase inst(clk,rst,clk_0,clk_90,clk_180,clk_270);
+  initial begin
+    clk=0;
+    forever #1 clk=~clk;
+  end
+  initial begin
+    rst=1;
+    #5 rst=0;
+    #50 $finish;
+  end
+endmodule
+      
