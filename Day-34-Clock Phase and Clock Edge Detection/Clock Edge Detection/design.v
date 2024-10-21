57ce057920fe7f43fa102ac8ab1820460d3b675a
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-21 15:38:37 +0530

    Update design.v

--- Day-34-Clock Phase and Clock Edge Detection/Clock Edge Detection/design.v
+++ Day-34-Clock Phase and Clock Edge Detection/Clock Edge Detection/design.v
@@ -1,1 +1,7 @@
-
+module edgedetection(clk,prev_clk,posedge_t,negedge_t,dual);
+  input wire clk,pre_clk;
+  output reg posedge_t,negedge_t,dual;
+  assign posedge_t = clk & (~prev_clk);
+  assign negedge_t = ~clk & prev_clk;
+  assign dual = clk ^ prev_clk;
+endmodule
