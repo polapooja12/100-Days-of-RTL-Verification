3b65797027742082256535af01e17c6478874098
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-22 20:46:37 +0530

    Update testbench.v

--- Day-35->Frequency Clock Divider/Frequency Clock Divider using D_flipflop/testbench.v
+++ Day-35->Frequency Clock Divider/Frequency Clock Divider using D_flipflop/testbench.v
@@ -2,10 +2,13 @@
 module clock_divider_test;
   reg clk,reset;
   wire f4;
-  clock_divider inst(clk,f4);
+  clock_divider inst(clk,reset,f4);
   always #5 clk=~clk;
   initial begin
     clk=0;
+    reset=1;
+    #5
+    reset=0;
     #500;
     $finish;
   end
