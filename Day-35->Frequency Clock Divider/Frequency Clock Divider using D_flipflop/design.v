1d44b96fc8a0ff42a0c22e125682860a85ed0312
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-22 20:43:10 +0530

    Update design.v

--- Day-35->Frequency Clock Divider/Frequency Clock Divider using D_flipflop/design.v
+++ Day-35->Frequency Clock Divider/Frequency Clock Divider using D_flipflop/design.v
@@ -1,1 +1,13 @@
-
+module d_flipflop(inut clk,reset,d, output reg q);
+  always @(posedge clk) begin
+    if(reset)
+      q<=0;
+    else
+      q<=d;
+  end
+endmodule
+module clock_divider(input clk,reset, output reg f4);
+  wire q2;
+  d_flipflop dff1(clk,reset,~f4,q2);
+  d_flipflop dff2(clk,reset,q2,f4);
+endmodule
