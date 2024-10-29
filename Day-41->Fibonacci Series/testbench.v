740edc84dcc3683a88c870a8b203372c2a8bc1e1
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-29 18:51:32 +0530

    Update testbench.v

--- Day-41->Fibonacci Series/testbench.v
+++ Day-41->Fibonacci Series/testbench.v
@@ -1,1 +1,14 @@
-
+`include "fibonacci_series.v"
+module fibonacci_series_test;
+  reg [31:0] n;
+  wire [31:0] result;
+  integer x;
+  fibonacci_series inst(n,result);
+  always for(x=0;x<=15;x=x+1)
+    #10 n=x;
+  initial begin
+    $monitor("Time=%0t n=%0d result=%0d",$time,n,result);
+    #160;
+    $finish;
+  end
+endmodule
