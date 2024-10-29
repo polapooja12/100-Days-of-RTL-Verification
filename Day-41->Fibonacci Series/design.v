f65f7600ee94bbfedc0160c96ddf1e7bde279d06
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-10-29 18:45:00 +0530

    Update design.v

--- Day-41->Fibonacci Series/design.v
+++ Day-41->Fibonacci Series/design.v
@@ -1,1 +1,13 @@
-
+module fib(input wire[31:0]n, output reg[31:0]result);
+  function automatic [31:0] fib;
+    inpu[31:0] x;
+    begin
+      if (x==0) fib=0;
+      else if (x<=2) fib=1;
+      else fib=fib(x-1)+fib(x-2);
+    end
+  endfunction
+  always @(*) begin
+    result=fib(n);
+  end
+endmodule
