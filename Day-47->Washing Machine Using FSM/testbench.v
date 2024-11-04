73a2d02e10277c5a2e4bb682df0578e2fb478f05
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-04 15:55:16 +0530

    Update testbench.v

--- Day-47->Washing Machine Using FSM/testbench.v
+++ Day-47->Washing Machine Using FSM/testbench.v
@@ -2,10 +2,10 @@
 module testbench(clk, rst, start, filled, doorclose, doorlock, detergent, cycletime_out, drained, spintime_out, fillvalve_on, soap_w ash, motor_on, drainvalve_on, water_wash, done);
 output reg clk, rst, doorclose, start, filled, detergent, cycletime_out, drained, spintime_out; 
 input wire doorlock, motor_on, fillvalve on, drainvalve_on, soapwash, water_wash, done;
-  washingmachine inst(clk, rst, start, filled, doorclose, doorlock, detergent, cycletime_out, drained, spintime_out, fillvalve_on, soap_wash, washmotor motor on, drainvalve on, water wash, done);
-  always #5 clk=-clk;
+  washingmachine inst(clk, rst, start, filled, doorclose, doorlock, detergent, cycletime_out, drained, spintime_out, fillvalve_on, soap_wash, motor_on, drainvalve_on, water_wash, done);
+  always #5 clk=~clk;
   initial begin
-    $monitor("Time=%od clk=%0b rst=%0b start=%0b doorclose=%0b,filled=%Ob detergent=%0b cycletime_out=%0b spintime_out=%0b doorlock=%ob fillvalve_on=%0b drainvalve_on=%eb soap wash=%ob water washob done=%0b", $time, clk, rst, start, doorclose, fi lled, detergent, cycletime_out, spintime out, doorlock, fillvalve on, drainvalve on, soap wash, water wash, done);
+    $monitor("Time=%0d clk=%0b rst=%0b start=%0b doorclose=%0b filled=%0b detergent=%0b cycletime_out=%0b spintime_out=%0b doorlock=%0b fillvalve_on=%0b drainvalve_on=%0b soap_wash=%0b water_wash=%0b done=%0b",$time, clk, rst, start, doorclose, filled, detergent, cycletime_out, spintime_out, doorlock, fillvalve_on, drainvalve_on, soap_wash, water_wash, done);
     clk=0;
     rst=1;
     start=0;
@@ -19,9 +19,9 @@ input wire doorlock, motor_on, fillvalve on, drainvalve_on, soapwash, water_wash
     #5 start=1; doorclose=1;
     #10 filled=1;
     #10 detergent=1;
-    #10 cycletime out=1;
+    #10 cycletime_out=1;
     #10 drained=1;
-    #10 spintime out=1;
+    #10 spintime_out=1;
     #50
     $finish;
   end
