4022d4e3f07c685a15b1cab428db48d5c4d73afc
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-06 23:07:14 +0530

    Update testbench.v

--- Day-49->Elevator/testbench.v
+++ Day-49->Elevator/testbench.v
@@ -14,7 +14,7 @@ module elevator_test(clk,rst,reqG,reqF1,reqF2,reqF3,overload,firealarm,person_de
     forever #5 clk=~clk;
   end
   initial begin
-    $monitor("Time=%0t | foor request:G=%0b F1=%0b F2=%0b F3=%0b|firealarm=%0b|overload=%0b|person_detected=%0b|position=%0b|door_open=%0b|door_close=%0b|bcd_floor=%0d|seg=%0b",$time,reqG,reqF1,reqF2,reqF3,firealarm,overload,person_detected,prox,door_open,door_closed,bcd_floor,seg);
+    $monitor("Time=%0t | floor request:G=%0b F1=%0b F2=%0b F3=%0b|firealarm=%0b|overload=%0b|person_detected=%0b|position=%0b|door_open=%0b|door_close=%0b|bcd_floor=%0d|seg=%0b",$time,reqG,reqF1,reqF2,reqF3,firealarm,overload,person_detected,prox,door_open,door_closed,bcd_floor,seg);
     rst=1;reqG=0;reqF1=0;regF2=0;reqF3=0;
     overload=0;firealarm=0;person_detected=0;
     #10 rst=0;
