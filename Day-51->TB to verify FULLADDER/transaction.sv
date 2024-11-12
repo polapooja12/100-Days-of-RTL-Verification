6d3af009ac43ab865a672dfc246e497ebde20ffc
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-12 11:58:12 +0530

    Update transaction.sv

--- Day-51->TB to verify FULLADDER/transaction.sv
+++ Day-51->TB to verify FULLADDER/transaction.sv
@@ -1,1 +1,10 @@
-
+class transaction;
+  rand bit a;
+  rand bit b;
+  rand bit c;
+  bit sum;
+  bit carry;
+  function void display(input string tag);
+    $display("Time:%0t [%0s]: a:%0b b:%0b c:%0b summ:%0b carry:%0b",$time, tag, a,b,c,sum,carry);
+  endfunction
+endclass
