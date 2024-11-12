55735f1ef0373166ca1dd8339f3a0d94baefb1f5
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-12 11:58:44 +0530

    Update writedriver.sv

--- Day-51->TB to verify FULLADDER/writedriver.sv
+++ Day-51->TB to verify FULLADDER/writedriver.sv
@@ -1,1 +1,18 @@
-
+class driver;
+  virtual intf.WR_DRV_MP wr_drv_inf;
+  transaction data2drv;
+  mailbox #(transaction)gen2wr;
+  function new(virtual intf.WR_DRV_MP  wr_drv_inf, mailbox #(transaction)gen2wr);
+    this.wr_drv_inf = wr_drv_inf;
+    this.gen2wr = gen2wr;
+  endfunction
+  virtual task drive();
+    forever begin
+    gen2wr.get(data2drv);
+     wr_drv_inf.a <= data2drv.a;
+        wr_drv_inf.b <= data2drv.b;
+        wr_drv_inf.c <= data2drv.c;
+        data2drv.display("DRV");
+    end
+  endtask
+endclass
