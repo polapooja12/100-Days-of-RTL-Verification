2bde020149e97f83d96fe4ff01b1c9b3f7400ec6
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-12 11:54:27 +0530

    Update interface.sv

--- Day-51->TB to verify FULLADDER/interface.sv
+++ Day-51->TB to verify FULLADDER/interface.sv
@@ -1,1 +1,11 @@
-
+interface intf(); 
+  logic a;
+  logic b;
+  logic c;
+  logic sum;
+  logic carry;
+  
+  modport WR_DRV_MP (output a,b,c);
+  modport WR_MON_MP (input a,b,c);
+  modport RD_MON_MP ( input sum,carry);
+endinterface
