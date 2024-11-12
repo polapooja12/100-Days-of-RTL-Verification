214839a7f11acaf1c93cfed21c4755646dc9d801
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-12 11:53:35 +0530

    Update generator.sv

--- Day-51->TB to verify FULLADDER/generator.sv
+++ Day-51->TB to verify FULLADDER/generator.sv
@@ -1,1 +1,20 @@
-
+class generator;
+  transaction gen_tr;
+  mailbox #(transaction) gen2wr;
+  function new(mailbox #(transaction) gen2wr);
+    this.gen2wr=gen2wr;
+    this.gen_tr=new();
+  endfunction
+  virtual task start();
+    fork
+      begin
+        repeat (5) begin
+          assert(gen_tr.randomize());
+          gen2wr.put(gen_tr);
+         gen_tr.display("GEN");
+          #5;
+        end
+      end
+    join_none
+  endtask
+endclass
