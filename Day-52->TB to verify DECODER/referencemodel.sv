edc205f84e949408489e7bf1d721e9c77dc923ff
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:35:09 +0530

    Update referencemodel.sv

--- Day-52->TB to verify DECODER/referencemodel.sv
+++ Day-52->TB to verify DECODER/referencemodel.sv
@@ -1,1 +1,25 @@
-
+class refmodel;
+  transaction wrmon_data;
+  mailbox #(transaction) wr2ref;
+  mailbox #(transaction) ref2scr;
+  function new (mailbox #(transaction)wr2ref, mailbox # (transaction) ref2scr);
+    this.wr2ref=wr2ref;
+    this.ref2scr=ref2scr;
+  endfunction
+  virtual task ref_logic(transaction wrmon_data);
+    wrmon_data.y1=~(wrmon_data.a)&~(wrmon_data.b);
+    wrmon_data.y2=~(wrmon_data.a)&(wrmon_data.b);
+    wrmon_data.y3=(wrmon_data.a)&~(wrmon_data.b);
+    wrmon_data.y4=(wrmon_data.a)&(wrmon_data.b);
+  endtask  
+  virtual task start();
+      forever 
+        begin
+        wrmon_data=new();
+        wr2ref.get(wrmon_data);
+          ref_logic(wrmon_data);
+          ref2scr.put(wrmon_data);
+          wrmon_data.display("refmodell");
+        end
+  endtask
+endclass
