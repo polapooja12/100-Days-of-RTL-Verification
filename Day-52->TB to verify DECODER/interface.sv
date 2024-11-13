9aedd4bf6332ebd80a80dd1508ef096e69fa8cc3
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:33:56 +0530

    Update interface.sv

--- Day-52->TB to verify DECODER/interface.sv
+++ Day-52->TB to verify DECODER/interface.sv
@@ -1,1 +1,11 @@
-
+interface intf(); 
+  logic a;
+  logic b;
+  logic y1;
+  logic y2;
+  logic y3;
+  logic y4; 
+  modport WR_DRV_MP (output a,b);
+  modport WR_MON_MP (input a,b);
+  modport RD_MON_MP ( input y1,y2,y3,y4);
+endinterface
