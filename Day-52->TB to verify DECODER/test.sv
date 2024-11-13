1bc626ac67d2892ca97247641ceaa8ae91a3fb0a
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:35:45 +0530

    Update test.sv

--- Day-52->TB to verify DECODER/test.sv
+++ Day-52->TB to verify DECODER/test.sv
@@ -1,1 +1,18 @@
-
+class test;
+  virtual intf.WR_DRV_MP wr_drv_inf;
+    virtual intf.WR_MON_MP wr_mon_inf;
+    virtual intf.RD_MON_MP rd_mon_inf;
+  environment env;
+  function new( virtual intf.WR_DRV_MP wr_drv_inf,
+    virtual intf.WR_MON_MP wr_mon_inf,
+    virtual intf.RD_MON_MP rd_mon_inf);
+     this.wr_drv_inf=wr_drv_inf;
+    this.wr_mon_inf=wr_mon_inf;
+    this.rd_mon_inf=rd_mon_inf;
+  env=new(wr_drv_inf,wr_mon_inf,rd_mon_inf);
+  endfunction
+  virtual task build_run();
+    env.build();
+    env.start();
+  endtask
+endclass
