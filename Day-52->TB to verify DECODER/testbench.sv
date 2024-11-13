35d9d057f683cbfb9d74076cf6a0b3b12b05d073
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:36:02 +0530

    Update testbench.sv

--- Day-52->TB to verify DECODER/testbench.sv
+++ Day-52->TB to verify DECODER/testbench.sv
@@ -1,1 +1,22 @@
-
+`include "design.sv"
+`include "interface.sv"
+`include "Transaction.sv"
+`include "generator.sv"
+`include "wr_driver.sv"
+`include "wr_mon.sv"
+`include "rd_mon.sv"
+`include "ref_model.sv"
+`include "scoreboard.sv"
+`include "env.sv"
+`include "test.sv"
+module top;
+  intf duv_if();
+  test test_h;
+  decoder dut(.a(duv_if.a), .b(duv_if.b), .y1(duv_if.y1), .y2(duv_if.y2), .y3(duv_if.y3), .y4(duv_if.y4));
+  initial begin
+    test_h=new(duv_if,duv_if,duv_if);
+    test_h.build_run();
+    #100;
+    $finish;
+  end
+endmodule
