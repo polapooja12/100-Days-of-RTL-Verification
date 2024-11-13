76b7b070d44a54f4bafb068fa7442bae8aad04bc
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:36:21 +0530

    Update transaction.sv

--- Day-52->TB to verify DECODER/transaction.sv
+++ Day-52->TB to verify DECODER/transaction.sv
@@ -1,1 +1,11 @@
-
+ class transaction;
+  rand bit a;
+  rand bit b;
+  bit y1;
+  bit y2;
+   bit y3;
+   bit y4;
+  function void display(input string tag);
+    $display("Time:%0t [%0s]: a:%0b b:%0b y1:%0b y2:%0b y3:%0b y4:%0b",$time, tag, a,b,y1,y2,y3,y4);
+  endfunction
+endclass
