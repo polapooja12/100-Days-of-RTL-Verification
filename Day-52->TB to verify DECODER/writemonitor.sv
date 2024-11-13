1b4f7c78e66a70a99cdb886f82e7da9e629d1d67
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-13 12:36:43 +0530

    Update writemonitor.sv

--- Day-52->TB to verify DECODER/writemonitor.sv
+++ Day-52->TB to verify DECODER/writemonitor.sv
@@ -1,1 +1,19 @@
-
+class monitor;
+  virtual intf.WR_MON_MP wr_mon_inf;
+  transaction  wrdata;//data2rm;
+  mailbox #(transaction)mon2rm;
+  function new(virtual intf.WR_MON_MP wr_mon_inf, mailbox #(transaction)mon2rm);
+    this.wr_mon_inf=wr_mon_inf;
+    this.mon2rm=mon2rm;
+    this.wrdata=new();
+  endfunction
+  virtual task monitor();
+    forever begin
+      #5;
+        wrdata.a = wr_mon_inf.a;
+        wrdata.b = wr_mon_inf.b;
+    wrdata.display("DATA FROM WRITE MONITOR");
+     mon2rm.put(wrdata);
+    end
+  endtask
+endclass
