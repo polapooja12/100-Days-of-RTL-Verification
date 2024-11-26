7b2b9218a17d6459469e397bd1aac949964b21c1
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-26 15:56:10 +0530

    Update logicgates_env.sv

--- Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_env.sv
+++ Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_env.sv
@@ -1,1 +1,45 @@
 
+`include "logicgates_tran.sv"
+`include "logicgates_gen.sv"
+`include "logicgates_drv.sv"
+`include "logicgates_mon.sv"
+`include "logicgates_scrb.sv"
+
+class environment;
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard scrb;
+
+  mailbox gen2drv;
+  mailbox gen2scr;
+  mailbox mon2scr;
+  virtual logicgates_inf vif;
+  function new(virtual logicgates_inf vif);
+    gen2drv = new();
+    gen2scr = new();
+    mon2scr = new();
+    
+    gen = new(gen2drv, gen2scr);
+    drv = new(gen2drv, vif);
+    mon = new(mon2scr, vif);
+    scrb = new(gen2scr, mon2scr);
+
+    this.vif = vif;
+    drv.vif = vif;
+    mon.vif = vif;
+  endfunction
+
+  task test();
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      scrb.run();
+    join_any
+  endtask
+
+  task run();
+    test();
+  endtask
+endclass
