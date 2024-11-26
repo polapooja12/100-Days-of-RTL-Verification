4ffd03d72fef88960e4ddf4496dad46a2f48988c
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-26 15:54:27 +0530

    Update logicgates_drv.sv

--- Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_drv.sv
+++ Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_drv.sv
@@ -1,18 +1,21 @@
-`include "logicgates_inf.sv"
-`include "logicgates_env.sv"
+class driver;
+  mailbox gen2drv;
+  transaction tr;
+  virtual logicgates_inf vif;
 
-module test_tb;
-  logicgates_inf inf();
-  logicgates_using_mux dut(.a(inf.a), .b(inf.b), .y1(inf.y1), .y2(inf.y2), .y3(inf.y3), .y4(inf.y4), .y5(inf.y5), .y6(inf.y6));
-  environment env;
-  initial begin
-    env = new(inf);
-    env.gen.n = 10; 
-    env.run();
+  function new(mailbox gen2drv, virtual logicgates_inf vif);
+    this.gen2drv = gen2drv;
+    this.vif = vif;
+  endfunction
+
+  task run();
+    forever begin
+      tr = new();
+      gen2drv.get(tr);
+      vif.a <= tr.a;
+      vif.b <= tr.b;
+      tr.print("DRIVER");
+      #1;
     end
-    initial begin
-      $dumpfile("dump.vcd");
-      $dumpvars;
-    #100 $finish;
-  end
-endmodule
+  endtask
+endclass
