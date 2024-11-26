8d731035c3b44f2fc13148a8c48a9adecff734de
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-26 15:53:53 +0530

    Update testbench.sv

--- Day-51->TB to verify logic gates using MULTIPLEXER/testbench.sv
+++ Day-51->TB to verify logic gates using MULTIPLEXER/testbench.sv
@@ -1,1 +1,18 @@
+`include "logicgates_inf.sv"
+`include "logicgates_env.sv"
 
+module test_tb;
+  logicgates_inf inf();
+  logicgates_using_mux dut(.a(inf.a), .b(inf.b), .y1(inf.y1), .y2(inf.y2), .y3(inf.y3), .y4(inf.y4), .y5(inf.y5), .y6(inf.y6));
+  environment env;
+  initial begin
+    env = new(inf);
+    env.gen.n = 10; 
+    env.run();
+    end
+    initial begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    #100 $finish;
+  end
+endmodule
