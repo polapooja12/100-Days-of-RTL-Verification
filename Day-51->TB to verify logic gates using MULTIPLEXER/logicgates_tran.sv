a60091d08f844ffc150a5355c5733ede9e0e0aa2
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-27 12:36:44 +0530

    Update logicgates_tran.sv

--- Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_tran.sv
+++ Day-51->TB to verify logic gates using MULTIPLEXER/logicgates_tran.sv
@@ -1,1 +1,10 @@
+class transaction;
+  rand bit a, b;
+  bit y1, y2, y3, y4, y5, y6;
 
+  function void print(string name);
+    $display("------- %0s -------", name);
+    $display("The values of a=%b and b=%b", a, b);
+    $display("Outputs: y1=%b, y2=%b, y3=%b, y4=%b, y5=%b, y6=%b", y1, y2, y3, y4, y5, y6);
+  endfunction
+endclass
