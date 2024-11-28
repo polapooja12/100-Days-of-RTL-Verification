c7b5a4d4b764593f73a0d8b0e2928f36d32feb32
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-28 12:08:48 +0530

    Create full_gen.sv

---
+++ Day-52->TB to verify FULLADDER/full_gen.sv
@@ -0,0 +1,22 @@
+class generator;
+int n;
+mailbox gen2drv; 
+mailbox gen2scr;
+transaction tr; 
+
+function new(mailbox gen2drv,mailbox gen2scr);
+this.gen2drv = gen2drv;
+this.gen2scr = gen2scr;
+endfunction
+
+task run();
+repeat(n)begin
+tr = new();
+tr.randomize();
+gen2drv.put(tr);
+gen2scr.put(tr);
+tr.print("GENERATOR");
+end
+
+endtask
+endclass
