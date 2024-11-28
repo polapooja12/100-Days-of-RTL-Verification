7a19693daa90e9a3a5904bbc9e52ee809e12aef4
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-11-28 12:08:08 +0530

    Update testbench.sv

--- Day-52->TB to verify FULLADDER/testbench.sv
+++ Day-52->TB to verify FULLADDER/testbench.sv
@@ -1,1 +1,22 @@
-
+`include "full_tran.sv"
+`include "full_gen.sv"
+`include "full_drv.sv"
+`include "full_mon.sv"
+`include "full_scrb.sv"
+`include "full_inf.sv"
+`include "full_env.sv"
+module test_tb;
+  add_inf inf();
+  full_adder dut(.a(inf.a),.b(inf.b),.cin(inf.cin),.sum(inf.sum),.carry(inf.carry));
+  environment env;
+  initial begin
+    env = new(inf);
+    env.gen.n=10;
+    env.run();
+  end
+    initial begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+      #100 $finish;
+  end
+endmodule
