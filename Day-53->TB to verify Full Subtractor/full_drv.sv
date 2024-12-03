952a161cc7f4852e78a0fa3974029d3f0d7d8a91
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:18:59 +0530

    Update full_drv.sv

--- Day-53->TB to verify Full Subtractor/full_drv.sv
+++ Day-53->TB to verify Full Subtractor/full_drv.sv
@@ -1,1 +1,21 @@
+class driver;
+    mailbox gen2drv;
+    transaction tr;
+    virtual sub_inf vif; 
+    function new(mailbox gen2drv, virtual sub_inf vif);
+        this.gen2drv = gen2drv;
+        this.vif = vif;
+    endfunction
 
+    task run();
+        forever begin
+            tr = new();
+            gen2drv.get(tr);
+            vif.a <= tr.a;
+            vif.b <= tr.b;
+            vif.bin <= tr.bin;
+            tr.print("DRIVER");
+            #1;
+        end
+    endtask
+endclass
