108e2fb4aa28e3b6c35876d0a58f0684682a0356
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:19:21 +0530

    Update full_mon.sv

--- Day-53->TB to verify Full Subtractor/full_mon.sv
+++ Day-53->TB to verify Full Subtractor/full_mon.sv
@@ -1,1 +1,25 @@
+class monitor;
+    transaction tra;
+    mailbox mon2scr; 
+    virtual sub_inf vif;
 
+    function new(mailbox mon2scr, virtual sub_inf vif);
+        this.mon2scr = mon2scr;
+        this.vif = vif;
+    endfunction
+
+    task run();
+        forever begin
+            tra = new();
+            tra.a = vif.a;
+            tra.b = vif.b;
+            tra.bin = vif.bin;
+            tra.diff = vif.diff;
+            tra.bout = vif.bout;
+
+            mon2scr.put(tra);
+            tra.print("MONITOR");
+            #1;
+        end
+    endtask
+endclass
