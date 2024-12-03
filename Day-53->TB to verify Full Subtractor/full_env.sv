d003ef9272c19375177e54812552e2f3d0abbc86
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:19:56 +0530

    Update full_env.sv

--- Day-53->TB to verify Full Subtractor/full_env.sv
+++ Day-53->TB to verify Full Subtractor/full_env.sv
@@ -1,1 +1,37 @@
 
+
+class environment;
+    driver drv;         
+    monitor mon; 
+    generator gen;
+    scoreboard scrb;    
+    mailbox gen2drv;    
+    mailbox gen2scr;    
+    mailbox mon2scr;    
+    virtual sub_inf vif; 
+    function new(virtual sub_inf vif);
+        gen2drv = new();  
+        gen2scr = new();  
+        mon2scr = new();  
+        gen = new(gen2drv, gen2scr);
+        drv = new(gen2drv, vif);
+        mon = new(mon2scr, vif);
+        scrb = new(gen2scr, mon2scr);
+
+        this.vif = vif;   
+        mon.vif = vif;    
+    endfunction
+
+    task test();
+        fork
+            gen.run();  
+            drv.run(); 
+            mon.run();  
+            scrb.run();  
+        join_any
+    endtask
+
+    task run();
+        test();  
+    endtask
+endclass
