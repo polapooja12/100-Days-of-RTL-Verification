386268a69014f51ea49ec1f4f12f745c318d7187
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:16:11 +0530

    Update testbench.sv

--- Day-53->TB to verify Full Subtractor/testbench.sv
+++ Day-53->TB to verify Full Subtractor/testbench.sv
@@ -1,1 +1,32 @@
+//`include "full_subtractor.sv"
+`include "full_tran.sv"
+`include "full_gen.sv"
+`include "full_drv.sv"
+`include "full_mon.sv"
+`include "full_scrb.sv"
+`include "full_inf.sv"
+`include "full_env.sv"
 
+module testbench;
+    sub_inf inf();
+    full_subtractor dut(
+        .a(inf.a),
+        .b(inf.b),
+        .bin(inf.bin),
+        .diff(inf.diff),
+        .bout(inf.bout)
+    );
+    environment env;
+
+    initial begin
+        env = new(inf);
+        env.gen.n = 10; 
+        env.run();
+      end
+
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars;
+#100 $finish;
+    end
+endmodule
