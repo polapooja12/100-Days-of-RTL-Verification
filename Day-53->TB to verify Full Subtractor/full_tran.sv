4ba346fb8e64e91269ffd448e7aa1174ffd3cf8f
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:18:04 +0530

    Update full_tran.sv

--- Day-53->TB to verify Full Subtractor/full_tran.sv
+++ Day-53->TB to verify Full Subtractor/full_tran.sv
@@ -1,1 +1,11 @@
+class transaction;
+    rand bit a, b, bin;
+    bit diff;
+    bit bout;
 
+    function void print(string name);
+        $display("------- %0s -------", name);
+        $display("The values of a=%b, b=%b, and bin=%b", a, b, bin);
+        $display("The diff=%b and bout=%b", diff, bout);
+    endfunction
+endclass
