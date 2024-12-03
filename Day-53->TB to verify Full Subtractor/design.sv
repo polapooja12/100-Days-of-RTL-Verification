acc3138da03f0cae583fa15a92591ad93cbc9fdf
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-03 12:15:45 +0530

    Update design.sv

--- Day-53->TB to verify Full Subtractor/design.sv
+++ Day-53->TB to verify Full Subtractor/design.sv
@@ -1,1 +1,6 @@
-
+module full_subtractor(input logic a, b, bin, output logic diff, bout);
+   assign diff = a ^ b ^ bin;
+assign bout = (~a & b) | (~a & bin) | (b & bin);
+  
+  //assign {bout, diff} = a - b - bin;
+endmodule
