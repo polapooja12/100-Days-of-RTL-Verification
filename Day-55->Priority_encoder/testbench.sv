6a6f7ca8af4c348f66870685ecbc623ebd69db3a
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-04 14:50:27 +0530

    Update testbench.sv

--- Day-55->Priority_encoder/testbench.sv
+++ Day-55->Priority_encoder/testbench.sv
@@ -1,1 +1,21 @@
+`include "priority_encoder_if.sv"
+`include "priority_encoder_env.sv"
 
+module test_tb;
+  priority_encoder_if inf();
+
+  priority_encoder dut(.a(inf.a), .b(inf.b), .c(inf.c), .d(inf.d), .y1(inf.y1), .y2(inf.y2));
+
+  environment env;
+
+  initial begin
+    env = new(inf);
+    env.gen.n = 10;  // Set the number of transactions
+    env.run();
+  end 
+  initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    #100 $finish;
+  end
+endmodule
