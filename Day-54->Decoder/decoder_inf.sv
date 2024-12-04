bb31f2e3faca7843526e3e4ccbfe9289a3ac7f72
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-04 14:36:33 +0530

    Create decoder_inf.sv

---
+++ Day-54->Decoder/decoder_inf.sv
@@ -0,0 +1,8 @@
+interface decoder_inf; // Interface for the decoder
+    logic a; // Input a
+    logic b; // Input b
+    logic y0; // Output y0
+    logic y1; // Output y1
+    logic y2; // Output y2
+    logic y3; // Output y3
+endinterface
