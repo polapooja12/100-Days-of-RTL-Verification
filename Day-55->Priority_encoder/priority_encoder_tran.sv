ef6e2026f09a6cbbdbd8ae905a249f80235e3d76
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-04 14:52:16 +0530

    Update priority_encoder_tran.sv

--- Day-55->Priority_encoder/priority_encoder_tran.sv
+++ Day-55->Priority_encoder/priority_encoder_tran.sv
@@ -1,1 +1,14 @@
+class transaction;
+  rand bit a;  // Input a
+  rand bit b;  // Input b
+  rand bit c;  // Input c
+  rand bit d;  // Input d
+  bit y1;      // Output y1
+  bit y2;      // Output y2
 
+  function void print(string name);
+    $display("------- %0s -------", name);
+    $display("Inputs: a=%b, b=%b, c=%b, d=%b", a, b, c, d);
+    $display("Outputs: y1=%b, y2=%b", y1, y2);
+  endfunction
+endclass
