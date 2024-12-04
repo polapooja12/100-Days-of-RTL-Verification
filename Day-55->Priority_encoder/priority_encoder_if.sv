e187d31237fc221c43535905eaf3967e76f123d5
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-04 14:51:27 +0530

    Create priority_encoder_if.sv

---
+++ Day-55->Priority_encoder/priority_encoder_if.sv
@@ -0,0 +1,8 @@
+interface priority_encoder_if;
+  logic a;   // Input a
+  logic b;   // Input b
+  logic c;   // Input c
+  logic d;   // Input d
+  logic y1;  // Output y1
+  logic y2;  // Output y2
+endinterface
