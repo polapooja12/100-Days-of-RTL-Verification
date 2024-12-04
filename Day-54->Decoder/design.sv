546a1e5017276260bc6ba557762c2ab25378b07a
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-04 14:44:14 +0530

    Update design.sv

--- Day-54->Decoder/design.sv
+++ Day-54->Decoder/design.sv
@@ -1,1 +1,6 @@
-
+module decoder(input logic a, b, output logic y0, y1, y2, y3);
+    assign y0 = ~a & ~b; // Output for 00
+    assign y1 = ~a & b;  // Output for 01
+    assign y2 = a & ~b;  // Output for 10
+    assign y3 = a & b;   // Output for 11
+endmodule
