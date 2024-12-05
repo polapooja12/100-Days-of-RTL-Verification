e7795c3f4faabe7e624ded0d945f865019f40577
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-05 12:48:13 +0530

    Create full_inf.sv

---
+++ Day-56->4x1-Multiplexer/full_inf.sv
@@ -0,0 +1,5 @@
+interface full_inf; // Interface for the MUX
+    logic a, b, c, d;   // 4 data inputs
+    logic s1, s2;       // 2-bit select input
+    logic y;            // Output
+endinterface
