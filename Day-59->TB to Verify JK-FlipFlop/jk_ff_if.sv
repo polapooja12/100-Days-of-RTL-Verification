445ab2090b9c6e0b285a8dda78736cea65d359f7
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-07 16:29:42 +0530

    Create jk_ff_if.sv

---
+++ Day-59->TB to Verify JK-FlipFlop/jk_ff_if.sv
@@ -0,0 +1,8 @@
+interface jk_ff_if;
+    logic clk;    // Clock signal
+    logic reset;  // Asynchronous reset signal
+    logic j;      // J input
+    logic k;      // K input
+    logic q;      // Q output
+    logic qn;     // Complementary Q output
+endinterface
