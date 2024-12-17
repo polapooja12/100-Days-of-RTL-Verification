22d3268af6191285ed3675e7a94efb516893dfd7
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-17 12:47:24 +0530

    Create sync_counter_interface.sv

---
+++ Day-62->TB to Verify Synchronous UP Counter using dff/sync_counter_interface.sv
@@ -0,0 +1,5 @@
+interface sync_counter_inf;
+    logic clk; 
+    logic [3:0] q;      // Output from the synchronous counter
+    logic [3:0] qbar;   // Inverted output from the synchronous counter
+endinterface
