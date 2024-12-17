e9c6bf311c514f7d6e00f1dc467909659e776af8
pola pooja <127873576+polapooja12@users.noreply.github.com>
2024-12-17 15:16:46 +0530

    Create clock_divider_interface.sv

---
+++ Day-63->TB to Verify Frequency Clock Divider/clock_divider_interface.sv
@@ -0,0 +1,9 @@
+interface clock_divider_inf;
+    logic clk;  // Clock signal
+    logic f2;   // Divided frequency signals
+    logic f4;
+    logic f8;
+    logic f16;
+    logic f32;
+    logic f64;
+endinterface
